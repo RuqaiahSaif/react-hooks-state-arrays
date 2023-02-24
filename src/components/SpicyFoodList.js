b8ea0f9a40540d046d4bf6a5eb8c29ff69b98b4d
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-02-25 00:54:35 +0300

    Completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,12 +6,33 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
-
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+  function handleUpdateiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      <button onClick={() => handleLiClick(food.id)}>x</button>
+      <button onClick={() => handleUpdateiClick(food.id)}>+</button>
+
     </li>
   ));
 
